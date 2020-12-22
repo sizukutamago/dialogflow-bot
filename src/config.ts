9d92ebdc7053a92cd2bf6864d506086176467df1
Asato Nago <nago@tam-bourine.co.jp>
2020-12-23 01:13:22 +0900

    refactor: 💡 changed env var & add consant values

---
+++ src/config.ts
@@ -0,0 +1,23 @@
+import * as config from "dotenv";
+
+config.config({ path: `envs/.env.${process.env.STAGE}` });
+
+Object.keys(config).forEach(key => {
+  process.env[key] = config[key];
+});
+
+export namespace General {
+  export const PORT = process.env.PORT as string;
+}
+
+export namespace Slack {
+  export const TOKEN = process.env.SLACK_BOT_TOKEN as string;
+  export const SECRET = process.env.SLACK_SIGNING_SECRET as string;
+  export const CHANNEL = process.env.SLACK_CHANNEL as string;
+  export const FAQ_BOT_ID = "<@U01F4UAKC>" as string;
+  export const APP_LISTEN_PORT = 8080 as number;
+}
+
+export namespace DialogFlow {
+  export const REGION = "Asia/Tokyo" as string;
+}
