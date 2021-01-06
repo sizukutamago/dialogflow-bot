b609a45a5dcd7ae02b41c12eee4a27e204787cf2
sizukutamago <sizukutamago@gmail.com>
2021-01-06 20:30:21 +0900

    dialogflow and bolt

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,38 @@
 import { App } from '@slack/bolt';
 import * as dotenv from 'dotenv';
-dotenv.config({ path: '../' });
+dotenv.config();
+
+const dialogflow = require('@google-cloud/dialogflow');
+const sessionClient = new dialogflow.SessionsClient();
+
+const projectId = process.env.GCP_PROJECT_ID || '';
+const sessionId = '123456';
+const languageCode = 'ja';
+
+
+async function detectIntent(
+    query: string
+) {
+    // The path to identify the agent that owns the created intent.
+    const sessionPath = sessionClient.projectAgentSessionPath(
+        projectId,
+        sessionId
+    );
+
+    // The text query request.
+    const request = {
+        session: sessionPath,
+        queryInput: {
+            text: {
+                text: query,
+                languageCode: languageCode,
+            },
+        },
+    };
+
+    const responses = await sessionClient.detectIntent(request);
+    return responses[0];
+}
 
 const app = new App({
     token: process.env.SLACK_BOT_TOKEN,
@@ -10,8 +42,9 @@ const app = new App({
 (async () => {
     await app.start(process.env.APP_LISTEN_PORT || 8080);
 
-    app.message('hello', async ({ message, say }) => {
+    app.message(/.*/, async ({ message, say }) => {
+        const intentResponse = await detectIntent(message.text || '');
         // say() sends a message to the channel where the event was triggered
-        await say(`Hey there <@${message.user}>!`);
+        await say(intentResponse.queryResult.fulfillmentText);
     });
 })();
